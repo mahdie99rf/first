cb60ea33e0c9f3aa43ebd91d35b580521c8cffd5
rasooli <mahdierasooli693@gmail.com>
2023-03-10 10:35:46 +0330

    creat book class

---
+++ src/Book.java
@@ -0,0 +1,18 @@
+ public abstract class Book {
+    String title;
+    double price;
+
+    public Book( String title){
+        this.title = title;
+    }
+
+     public String getTitle() {
+         return title;
+     }
+
+     public double getPrice() {
+         return price;
+     }
+
+     public abstract void setPrice(double price);
+ }
