06f90a6fe5f5215fffa9bec69a30313427f1efec
sarabaghani <sarabaghani72@gmail.com>
2023-03-10 13:31:55 +0600

    create usebook

---
+++ src/UseBook.java
@@ -0,0 +1,8 @@
+public class UseBook {
+    public static void main(String[] args) {
+        Fiction fiction = new Fiction("Harry potter");
+        NonFiction nonFiction = new NonFiction("Oliver Twist");
+        System.out.println(fiction);
+        System.out.println(nonFiction);
+    }
+}
